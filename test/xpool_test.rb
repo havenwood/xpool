4970ee888f0d3d2f1e8c78fda32dcf2d726346ac
Robert Gleeson <rob@flowof.info>
2012-12-01 01:04:57 +0100

    Add queue test. Closes #3

--- test/xpool_test.rb
+++ test/xpool_test.rb
@@ -6,6 +6,26 @@ class XPoolTest < Test::Unit::TestCase
     end
   end
 
+  class XUnit
+    def initialize
+      file = Tempfile.new '__xpool_test'
+      @path = file.path
+      file.close
+    end
+
+    def run
+      File.open @path, 'w' do |f|
+        f.write 'true'
+      end
+    end
+
+    def run?
+      return @run if defined?(@run)
+      @run = File.read(@path) == 'true'
+      FileUtils.rm_rf @path
+    end
+  end
+
   def setup
     @pool = XPool.new 5
   end
@@ -14,6 +34,19 @@ class XPoolTest < Test::Unit::TestCase
     @pool.shutdown
   end
   
+
+  def test_queue 
+    @pool.resize! 1..1
+    units = Array.new(5) { XUnit.new }
+    units.each do |unit|
+      @pool.schedule unit
+    end
+    @pool.shutdown
+    units.each do |unit|
+      assert unit.run?
+    end
+  end
+
   def test_parallelism 
     5.times do 
       @pool.schedule Unit.new
