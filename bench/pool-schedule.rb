0e8235f5ceb61b4b77cd3e0e2305a6ebcd41c61a
Robert Gleeson <rob@flowof.info>
2013-03-17 06:20:35 +0100

    add schedule bench. closes #14

--- bench/pool-schedule.rb
+++ bench/pool-schedule.rb
@@ -1,18 +1,15 @@
 require "xpool"
 require "ruby-prof"
 class Unit
-  def run(x)
+  def run(x = 1)
     sleep x
   end
 end
 
-pool = nil
+pool = XPool.new 5
 result = RubyProf.profile do
-  pool = XPool.new 5
-  5.times { pool.schedule Unit.new, 0.1 }
-  pool.dry?
+  5.times { pool.schedule Unit.new }
 end
-
 pool.shutdown
 printer = RubyProf::FlatPrinter.new(result)
-printer.print(STDOUT)
+printer.print
